931f1570c0d2f394a4941b065dbac9920d3b2f3b
Vitaly-Stefanyshyn777 <167921099+Vitaly-Stefanyshyn777@users.noreply.github.com>
2025-01-19 20:43:47 +0200

    Task-2

--- src/server.js
+++ src/server.js
@@ -72,3 +72,56 @@
 //     console.log(`Server is running on port ${PORT}`);
 //   });
 // };
+
+// import express from "express";
+// import cors from "cors";
+// import pino from "pino-http";
+
+// const setupServer = async () => {
+//   const app = express();
+
+//   app.use(express.json());
+//   app.use(cors());
+//   app.use(
+//     pino({
+//         transport: {
+//             target: 'pino-pretty',
+//         }
+
+//     })
+//   );
+// };
+
+// src/server.js
+import express from "express";
+import cors from "cors";
+import pino from "pino-http";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const setupServer = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+  app.use(
+    pino({
+      transport: {
+        target: "pino-pretty",
+      },
+    })
+  );
+
+  app.use("*", (req, res) => {
+    res.status(404).json({
+      message: "Not found",
+    });
+  });
+
+  const PORT = process.env.PORT || 3000;
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
