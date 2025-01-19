da5b1bb068ec3e9e57f5c1a26165c365a8a5b9f6
Vitaly-Stefanyshyn777 <167921099+Vitaly-Stefanyshyn777@users.noreply.github.com>
2025-01-19 21:04:55 +0200

    Task-5

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,8 +1,20 @@
-import { ContactsCollection } from '../db/models/contact.js';
+// import { ContactsCollection } from '../db/models/contact.js';
+
+// export const getAllContacts = async () => {
+//   return await ContactsCollection.find();
+// };
+// export const getContactById = async (contactId) => {
+//   return await ContactsCollection.findById(contactId);
+// };
+
+// src/services/contacts.js
+import { Contact } from "../models/Contact.js";
 
 export const getAllContacts = async () => {
-  return await ContactsCollection.find();
-};
-export const getContactById = async (contactId) => {
-  return await ContactsCollection.findById(contactId);
+  try {
+    const contacts = await Contact.find(); // Отримуємо всі контакти з бази даних
+    return contacts;
+  } catch (error) {
+    throw new Error("Failed to fetch contacts: " + error.message);
+  }
 };
