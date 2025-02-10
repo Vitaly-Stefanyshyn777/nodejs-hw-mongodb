73d479583dfe731961039a43256149f3a5b03032
Vitaly-Stefanyshyn777 <167921099+Vitaly-Stefanyshyn777@users.noreply.github.com>
2025-02-10 15:59:52 +0200

    Додаю перевірку

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
-import { setupServer } from "./server.js";
-import { initMongoConnection } from "./db/initMongoConnection.js";
+// import { setupServer } from "./server.js";
+// import { initMongoConnection } from "./db/initMongoConnection.js";
 
-const bootstrap = async () => {
-  await initMongoConnection();
-  setupServer();
-};
+// const bootstrap = async () => {
+//   await initMongoConnection();
+//   setupServer();
+// };
 
-bootstrap();
+// bootstrap();
+import express from "express";
+
+const app = express();
+
+// Створіть обробник для кореневого маршруту '/'
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+// Запустіть сервер
+app.listen(3021, () => {
+  console.log("Server is running on port 3021");
+});
